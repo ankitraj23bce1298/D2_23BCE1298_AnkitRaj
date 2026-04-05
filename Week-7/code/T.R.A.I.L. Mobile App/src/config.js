37e21e22ef7b93eaa54de7862eebbef5d13fbcda
ankitraj23bce1298 <ankit.raj2023@vitstudent.ac.in>
2026-04-06 00:12:09 +0530

    Delete Week-7 directory

--- Week-7/code/T.R.A.I.L. Mobile App/src/config.js
+++
@@ -1,3 +0,0 @@
-// // This file contains your secret API key.
-// // ✨ IMPORTANT: Add this file's name to your .gitignore file!
-// export const GEMINI_API_KEY = "AIzaSyBzfUEn4Lcop5ivNuUGG4KcfmurH42GZkw";
